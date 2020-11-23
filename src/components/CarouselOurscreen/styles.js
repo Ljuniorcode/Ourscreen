b9a1b4fbaff76d990d06fc340a202b88640aa7c7
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-11-23 12:00:16 -0300

    finalizando aula 05 - arrow

--- src/components/CarouselOurscreen/styles.js
+++ src/components/CarouselOurscreen/styles.js
@@ -1,24 +1,47 @@
 import styled from 'styled-components'
-import { Background, WrapperThumb } from '../ThumbOurscreen/styles';
+import { Background, WrapperThumb } from '../ThumbOurscreen/styles'
+import arrow from '../../assets/img/arrow.svg'
 
 export const Right = styled.button`
   position:absolute;
   height:calc(100% - var(--space-top-bottom) * 2);
   width:50rem;
-  background-color:red;
+  background-color:var(--color-old-silver);
   right:0;
   opacity:0;
+  border:none;
+  transition:opacity 200ms linear, transform 200ms linear;
+  cursor:pointer;
+
+  &::after {
+    content: url(${arrow});
+    display:inline-block;
+    opacity:1;
+    transform:rotate(-90deg);
+  }
+
+  &:hover::after {
+    transform: scale(1.2) rotate(-90deg) ;
+  }
+
+  &:active::after {
+    transition:transform 100ms linear;
+    transform:rotate(-90deg) scale(1.2) translateY(4rem);
+  }
 `;
 
 export const CarouselStyle = styled.div`
   --space-top-bottom:20rem;
   position:relative;
   display:flex;
-  align-self:center;
-  box-sizing:0;
+  align-items:center;
+  align-self:flex-start;
+  box-sizing:border-box;
   width:100%;
   padding:var(--space-top-bottom) 30rem;
   overflow: hidden;
+
+ 
 
   & > ${Background}{
     margin-right:20rem;
@@ -29,6 +52,11 @@ export const CarouselStyle = styled.div`
   }
 
   &:hover > ${Right}{
-    opacity:1;
+    opacity:0.8;
+  }
+
+  & > ${Right}{
+    opacity:0.9;
+    transform-origin: right center;
   }
 `;
